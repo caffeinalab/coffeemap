d265a1d4a5263c634236c954d2ad03dc822270eb
Flavio De Stefano <flavio.destefano@caffeinalab.com>
2015-07-23 09:12:51 +0200

    Fixes of Thu Jul 23 09:12:51 CEST 2015

--- coffeemap.js
+++ coffeemap.js
@@ -1,8 +1,15 @@
+"use strict";
+
 function CoffeeMap(opt) {
 	var self = this;
-	opt = _.extend({
 
-		containerSelector: 'body',
+	if (opt.containerSelector == null) {
+		throw new Error('CoffeeMap: define a container selector');
+	}
+
+	self.opt = _.defaults(opt, {
+
+		containerSelector: null,
 		mapName: 'world',
 		baseSVGPath: '/svg',
 
@@ -16,87 +23,159 @@ function CoffeeMap(opt) {
 			console.log('Clicked', obj_country);
 		}
 
-	}, opt);
+	});
 
 
-	/**
-	 * @property $container
-	 * @type {Object}
-	 */
-	self.$container = $(opt.containerSelector);
-	self.$container.addClass('coffeemap-container');
+	self.paths = {};
 
+	self.$container = jQuery(self.opt.containerSelector).addClass('coffeemap-container');
 
-	/**
-	 * @property parsableCountries
-	 * @type {Object}
-	 */
-	self.parsableCountries = {};
+	self.$container.on('mouseenter', 'path', function(e) {
+		var $path = jQuery(e.target);
+		if ($path.attr('highlighted') == null) return;
 
-	var parseCountry = function(code, attribute) {
-		var $dom = self._svg.find('#' + code);
-		if ($dom.length > 0) {
-			$dom.attr(attribute, true);
-			if (!self.parsableCountries[code]) {
-				self.parsableCountries[code] = { code : code };
-				$dom.find('> desc > *').each(function() {
-					self.parsableCountries[code][ this.tagName ] = $(this).html();
-				});
+		// Get the country object
+		var id = $path[0].id;
+		self.$tooltip.referral = id;
+		var obj_country = self.paths[ id ];
+
+		// Calculate the best position for the tooltip
+		var path_rect = $path[0].getBoundingClientRect();
+		var path_offset = $path.offset();
+		var map_offset = self.$svg.offset();
+		var bounds = {
+			left: path_offset.left - map_offset.left,
+			top: path_offset.top - map_offset.top,
+			width: path_rect.width,
+			height: path_rect.height
+		};
+
+		// Call the function the will populate the tooltip
+		self.opt.highlightCallback.call(self, obj_country);
+
+		var x = bounds.left + ((obj_country['hc-middle-x'] || 0.5) * bounds.width) - (self.$tooltip.width() / 2) - 10;
+		var y = bounds.top + ((obj_country['hc-middle-y'] || 0.5) * bounds.height) + 10;
+
+		// Set the "parent" of tooltip
+		self.$tooltip.uiShow({ top: y, left: x });
+	});
+
+	///////////////////////
+	// Mouse leave path //
+	///////////////////////
+
+	self.$container.on('mouseleave', 'path', function(e) {
+		var isOverOn = jQuery(e.target)[0].id;
+		var wasOverOn = self.$tooltip.referral;
+
+		self.$tooltip.referral = false;
+
+		// Actually "hide" the tooltip after 500ms
+		self.$tooltip.hideTimeout = setTimeout(self.$tooltip.uiHide, 500);
+	});
+
+	////////////////////
+	// Click on path //
+	////////////////////
+
+	self.$container.on('click', '.coffeemap-map path', function(e) {
+		var $path = jQuery(e.target);
+		var id = $path[0].id;
+
+		if ($path.attr('clickable') != null) {
+			if (_.isFunction(self.opt.clickableCallback)) {
+				var obj_country = self.paths[ id ];
+				self.opt.clickableCallback.call(self, obj_country);
 			}
 		}
-	};
+	});
+
+	///////////////////////////
+	// Click on the mobilist //
+	///////////////////////////
+
+	self.$container.on('click', '.coffeemap-mobilist li', function(e) {
+		var $li = jQuery(e.target);
+		var id = $li.data('id');
+
+		if (_.isFunction(self.opt.clickableCallback)) {
+			var obj_country = self.paths[ id ];
+			self.opt.clickableCallback.call(self, obj_country);
+		}
+	});
 
 
-	/**
-	 * @method buildAndAppendSVG
-	 * @param  {Object} xhr
-	 */
-	self.buildAndAppendSVG = function(xhr) {
-		self._svg = $(xhr.responseXML.documentElement);
-		self._svg.attr({
-			"class": 'coffeemap-map',
-			width: '100%',
-			height: '100%',
-			preserveAspectRatio: true,
-			viewBox: '0 0 700 700'
-		});
+	/////////////////////////////////////////////////
+	// Get the SVG async and append all the things //
+	/////////////////////////////////////////////////
 
-		// Add clickable and hightlighed countries
-		_.each(opt.clickableCountries, function(c) { parseCountry(c, 'clickable'); });
-		_.each(opt.highlightedCountries, function(c) { parseCountry(c, 'highlighted'); });
+	$.ajax({
+		url: self.opt.baseSVGPath + '/' + self.opt.mapName + '.svg',
+		dataType: 'xml',
+		success: function(svgContent, status, xhr) {
+			self.buildAndAppendSVG(xhr);
+			self.buildAndAppendMobilist();
+			self.buildAndAppendTooltip();
+		}
+	});
 
-		// Add effect. to the DOM
-		self.$container.append( self._svg );
-	};
+}
 
-	/**
-	 * @method buildAndAppendMobilist
-	 */
-	self.buildAndAppendMobilist = function() {
-		var $mobilist = $('<ul/>', {
-			"class": 'coffeemap-mobilist',
-		});
+CoffeeMap.prototype.parseCountry = function(code, attribute) {
+	var self = this;
 
-		_.each(opt.clickableCountries, function(id) {
-			$mobilist.append($('<li/>', {
-				html: self.parsableCountries[id].name,
-				"data-id": id
-			}));
-		});
+	var $dom = self.$svg.find('#' + code);
+	if ($dom.length > 0) {
+		$dom.attr(attribute, true);
+		if (!self.paths[code]) {
+			self.paths[code] = { code : code };
+			$dom.find('> desc > *').each(function() {
+				self.paths[code][ this.tagName ] = jQuery(this).html();
+			});
+		}
+	}
+};
 
-		self.$container.append( $mobilist );
-	};
+CoffeeMap.prototype.buildAndAppendSVG = function(xhr) {
+	var self = this;
 
+	self.$svg = jQuery(xhr.responseXML.documentElement);
+	self.$svg.attr({
+		"class": 'coffeemap-map',
+		width: '100%',
+		preserveAspectRatio: true,
+	});
 
-	////////////////////////
-	// Tooltip generator //
-	////////////////////////
+	// Add clickable and hightlighed countries
+	_.each(self.opt.clickableCountries, function(c) { self.parseCountry(c, 'clickable'); });
+	_.each(self.opt.highlightedCountries, function(c) { self.parseCountry(c, 'highlighted'); });
 
-	/**
-	 * @property $tooltip
-	 * @type {DOM}
-	 */
-	self.$tooltip = $('<div/>', {
+	// Add effect. to the DOM
+	self.$container.append( self.$svg );
+};
+
+CoffeeMap.prototype.buildAndAppendMobilist = function() {
+	var self = this;
+
+	var $mobilist = jQuery('<ul/>', {
+		"class": 'coffeemap-mobilist',
+	});
+
+	_.each(self.opt.clickableCountries, function(id) {
+		$mobilist.append(jQuery('<li/>', {
+			html: self.paths[id].name,
+			"data-id": id
+		}));
+	});
+
+	// Add effect. the mobilist
+	self.$container.append( $mobilist );
+};
+
+CoffeeMap.prototype.buildAndAppendTooltip = function() {
+	var self = this;
+
+	self.$tooltip = jQuery('<div/>', {
 		'class': 'coffeemap-tooltip',
 	});
 
@@ -131,93 +210,23 @@ function CoffeeMap(opt) {
 		}
 	);
 
+	// add effect. the tooltip
 	self.$container.append( self.$tooltip );
+};
 
+////////////////////////
+// jQuery integration //
+////////////////////////
 
-	///////////////////////
-	// Mouse enter path //
-	///////////////////////
-
-	self.$container.on('mouseenter', 'path', function(e) {
-		var $path = $(e.target);
-		if ($path.attr('highlighted') == null) return;
-
-		// Get the country object
-		var id = $path[0].id;
-		self.$tooltip.referral = id;
-
-		var obj_country = self.parsableCountries[ id ];
-
-		// Calculate the best position for the tooltip
-		var bounds = $path[0].getBoundingClientRect();
-		var offset = $path.offset();
-
-		var offset_map = self.$container.find('.coffeemap-map').offset();
-		offset.left -= offset_map.left;
-		offset.top -= offset_map.top;
-
-		// Call the function the will populate the tooltip
-		opt.highlightCallback.call(self, obj_country);
-
-		// Set the "parent" of tooltip
-		self.$tooltip.uiShow({
-			top: offset.top + bounds.height + 10,
-			left: offset.left + (bounds.width / 2) - (self.$tooltip.width() / 2)
-		});
+jQuery.fn.coffeeMap = function() {
+	jQuery(this).each(function() {
+		var $t = jQuery(this);
+		$t.data('coffeemap', new CoffeeMap({
+			containerSelector: $t,
+			mapName: $t.data('map-name'),
+			baseSVGPath: $t.data('base-svg-path'),
+			highlightedCountries: $t.data('highlighted-countries'),
+			clickableCountries: $t.data('clickable-countries')
+		}));
 	});
-
-	///////////////////////
-	// Mouse leave path //
-	///////////////////////
-
-	self.$container.on('mouseleave', 'path', function(e) {
-		var isOverOn = $(e.target)[0].id;
-		var wasOverOn = self.$tooltip.referral;
-
-		self.$tooltip.referral = false;
-
-		// Actually "hide" the tooltip after 500ms
-		self.$tooltip.hideTimeout = setTimeout(self.$tooltip.uiHide, 500);
-	});
-
-	////////////////////
-	// Click on path //
-	////////////////////
-
-	self.$container.on('click', '.coffeemap-map path', function(e) {
-		var $path = $(e.target);
-		var id = $path[0].id;
-
-		if ($path.attr('clickable') != null) {
-			if (_.isFunction(opt.clickableCallback)) {
-				var obj_country = self.parsableCountries[ id ];
-				opt.clickableCallback.call(self, obj_country);
-			}
-		}
-	});
-
-	self.$container.on('click', '.coffeemap-mobilist li', function(e) {
-		var $li = $(e.target);
-		var id = $li.data('id');
-
-		if (_.isFunction(opt.clickableCallback)) {
-			opt.clickableCallback.call(self, id);
-		}
-	});
-
-	// Maps are taken from http://code.highcharts.com/mapdata/1.0.0/
-
-	/////////////////////////
-	// Load the SVG async //
-	/////////////////////////
-
-	$.ajax({
-		url: opt.baseSVGPath + '/' + opt.mapName + '.svg',
-		dataType: 'xml',
-		success: function(svgContent, status, xhr) {
-			self.buildAndAppendSVG(xhr);
-			self.buildAndAppendMobilist();
-		}
-	});
-
-}
+};
